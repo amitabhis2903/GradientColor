9860dc3a1373d2e8347b58c935555089b85c394e
Amitabh Pandey <amitpandey-tcs-93085@Amits-MacBook-Air.local>
2020-05-16 23:32:39 +0530

    Finish Gradient Color Framework For Swift

---
+++ GradientColor/GradientColor/GradientColor.h
@@ -0,0 +1,20 @@
+//
+//  GradientColor.h
+//  GradientColor
+//
+//  Created by Amitabh Pandey on 15/05/20.
+//  Copyright © 2020 AmitabhPandey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+//! Project version number for GradientColor.
+FOUNDATION_EXPORT double GradientColorVersionNumber;
+
+//! Project version string for GradientColor.
+FOUNDATION_EXPORT const unsigned char GradientColorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GradientColor/PublicHeader.h>
+
+
---
+++ GradientColor/GradientColor/GradientColor.swift
@@ -0,0 +1,24 @@
+//
+//  GradientColor.swift
+//  GradientColor
+//
+//  Created by Amitabh Pandey on 15/05/20.
+//  Copyright © 2020 AmitabhPandey. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    public func setGradientColor(colorOne: UIColor, colorTwo: UIColor) {
+           
+           let gradientLayer = CAGradientLayer()
+           gradientLayer.frame = bounds
+           gradientLayer.colors = [colorOne.cgColor, colorTwo.cgColor]
+           gradientLayer.locations = [0.0, 1.0]
+           gradientLayer.startPoint = CGPoint(x: 1.0, y: 1.0)
+           gradientLayer.endPoint = CGPoint(x: 0.0, y: 0.0)
+           layer.insertSublayer(gradientLayer, at: 0)
+       }
+    
+}
